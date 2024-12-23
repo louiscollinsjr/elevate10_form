9f55a25e9326f5dd9df74b336863023ff0eb128a
louiscollinsjr <louiscollinsjr@gmail.com>
2024-12-23 16:44:39 +0200

    update meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 import MultiStepForm from '@/components/MultiStepForm';
 
 export const metadata = {
-  title: 'Web Design Project Form',
-  description: 'Start your web design project by filling out our detailed questionnaire',
+  title: 'Introducing Elevate10+ by atem.',
+  description: 'Start your web design project by filling out our startup form.',
 };
 
 export default function Home() {
